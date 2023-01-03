d2ccaf40afb85e82da5e29bd5de0773b1c4351d9
pankhuri1808 <spankhuri1808@gmail.com>
2023-01-04 00:42:07 +0530

    first commit-mernchat

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Homepage from './Pages/Homepage';
+import Chatpage from './Pages/Chatpage';
+import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+   <Route path="/" component={Homepage} exact/>
+   <Route path="/chats" component={Chatpage} exact/>
+
+    </div>
+  );
+}
+
+export default App;
